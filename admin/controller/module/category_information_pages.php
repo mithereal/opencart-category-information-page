70e0b2cdb7ddbe1c5b62b49bd7b00472e5b4866a
Jason Clark <mithereal@gmail.com>
2014-09-17 14:35:22 -0700

    added module index page

--- admin/controller/module/category_information_pages.php
+++ admin/controller/module/category_information_pages.php
@@ -1,11 +1,43 @@
 <?php
-class ControllerModulecategory_information_pages extends Controller {
+class ControllerModulecategoryinformationpages extends Controller {
 	private $error = array(); 
 	
 	public function index() {
 		$this->language->load('module/category_information_pages');
 
 		$this->document->setTitle($this->language->get('heading_title'));
+		$this->data['breadcrumbs'] = array();
+
+   		$this->data['breadcrumbs'][] = array(
+       		'text'      => $this->language->get('text_home'),
+			'href'      => $this->url->link('common/home', 'token=' . $this->session->data['token'], 'SSL'),
+      		'separator' => false
+   		);
+
+   		$this->data['breadcrumbs'][] = array(
+       		'text'      => $this->language->get('text_module'),
+			'href'      => $this->url->link('extension/module', 'token=' . $this->session->data['token'], 'SSL'),
+      		'separator' => ' :: '
+   		);
+
+   		$this->data['breadcrumbs'][] = array(
+       		'text'      => $this->language->get('heading_title'),
+			'href'      => $this->url->link('module/category_information_pages', 'token=' . $this->session->data['token'], 'SSL'),
+      		'separator' => ' :: '
+   		);
+   		$this->data['heading_title'] = $this->language->get('heading_title');
+   		$this->data['button_save'] = $this->language->get('button_save');
+		$this->data['button_cancel'] = $this->language->get('button_cancel');
+		$this->data['entry_success'] = $this->language->get('entry_success');
+		$this->data['entry_description'] = $this->language->get('entry_description');
+		
+		$this->template = 'module/category_information_pages.tpl';
+		$this->children = array(
+			'common/header',
+			'common/footer'
+		);
+
+		$this->response->setOutput($this->render());
 		
 	}
 	
--- admin/language/english/module/category_information_pages.php
+++ admin/language/english/module/category_information_pages.php
@@ -9,12 +9,16 @@ $_['text_content_top']    = 'Content Top';
 $_['text_content_bottom'] = 'Content Bottom';
 $_['text_column_left']    = 'Column Left';
 $_['text_column_right']   = 'Column Right';
+$_['button_save']   = 'Save';
+$_['button_cancel']   = 'Cancel';
 
 // Entry
 $_['entry_layout']        = 'Layout:';
 $_['entry_position']      = 'Position:';
 $_['entry_status']        = 'Status:';
 $_['entry_sort_order']    = 'Sort Order:';
+$_['entry_success']    = 'Successfully Installed:';
+$_['entry_description']    = 'To Create Information Pages add them to the informations section, there will be a select option in the Category Selection >> category >> edit >> Data';
 
 // Error
 $_['error_permission']    = 'Warning: You do not have permission to modify module ategory information pages!';
